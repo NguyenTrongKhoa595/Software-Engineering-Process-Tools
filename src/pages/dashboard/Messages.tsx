c82e03ffa55520d04c21a156c017c7fac8bcd673
Khoa Nguyen <khoa.nt200804@gmail.com>
2025-12-26 18:51:08 +0700

    add route messages by user role

---
+++ src/pages/dashboard/Messages.tsx
@@ -0,0 +1,13 @@
+import { useAuth } from '@/contexts/AuthContext';
+import LandlordMessages from './landlord/LandlordMessages';
+import TenantMessages from './tenant/TenantMessages';
+
+export default function Messages() {
+  const { user } = useAuth();
+
+  if (user?.role === 'landlord') {
+    return <LandlordMessages />;
+  }
+
+  return <TenantMessages />;
+}
